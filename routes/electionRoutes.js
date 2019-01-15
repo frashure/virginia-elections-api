4579c8b724b6c23654e11a299c61e6e44aad4469
Chris Frashure <csfrashure@gmail.com>
2019-01-14 19:43:57 -0500

    added routes for getting elections by office and year

--- routes/electionRoutes.js
+++ routes/electionRoutes.js
@@ -3,5 +3,13 @@ const router = express.Router();
 const controller = require('../controllers/electionsController');
 
 router.get('/', controller.getAllElections);
+router.get('/:office', controller.getElectionsByOffice);
+router.get('/:year', controller.getElectionsByYear);
+
+/*
+    TODO: get elections by congressional district,
+    need to pass params through Google Civic Info API,
+    parse OCID for finding districts attached to address
+*/
 
 module.exports = router;
