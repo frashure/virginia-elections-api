98fe24ec2d9df3de5cb2e864a9458513a3d3a834
Chris Frashure <csfrashure@gmail.com>
2019-01-14 19:42:35 -0500

    added additional get functions

--- controllers/candidatesController.js
+++ controllers/candidatesController.js
@@ -6,12 +6,32 @@ var controller = {
         var results = db.query('SELECT * FROM candidates', (err, results) => {
             if (err) {
                 console.log(err);
+                res.send(err);
             }
             else {
                 res.json(results);
             }
         });
-    } // end getAllCandidates
+    }, // end getAllCandidates
+
+    getCandidatesByOffice: (req, res) => {
+        let results = db.query(`select c.first_name, c.last_name, c.party_id, c.website
+        from election_candidates ec
+            left join candidates c 
+                on ec.candidate_id = c.candidate_id
+        where election_id in (
+            select election_id
+            from elections
+            where office_id = ?)`, [req.params.office], (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+                else {
+                    res.json(results);
+                }
+            })
+    }
 
 }; // end controller object
 
