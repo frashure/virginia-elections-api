62d7975a874418c17b9ef921e3fcb5389c76abe9
Chris Frashure <csfrashure@gmail.com>
2019-01-15 15:30:42 -0500

    added logging during db reconnect

--- models/db.js
+++ models/db.js
@@ -22,8 +22,9 @@ function handleDisconnect() {
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();  
+      console.log('handling disconnect...');                       // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
     }
