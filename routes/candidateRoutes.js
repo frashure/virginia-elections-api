0ce8fb5381f73f199857b474f91788a8f0845323
Chris Frashure <csfrashure@gmail.com>
2019-01-26 15:37:54 -0500

    new routes for candidates and their elections, candidates by address

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/candidatesController');
 
-
-router.get('/address/:address', controller.getCandidatesByAddress);
 router.get('/', controller.getAllCandidates);
+router.get('/elections', controller.getCandidatesAndTheirElections);
+router.get('/address/:address', controller.getCandidatesByAddress);
 router.get('/:office', controller.getCandidatesByOffice);
 router.get('/:office/:district', controller.getCandidatesByDistrict);
-
-/*
-    TODO: get candidates by congressional district,
-    need to pass params through Google Civic Info API,
-    parse OCID for finding districts attached to address
-*/
 
 module.exports = router;
