b0fd24ac2ecd28db098d8326e40938a08bcb7352
Chris Frashure <csfrashure@gmail.com>
2019-01-27 18:02:47 -0500

    added method to return elections with array of candidates in election

--- controllers/electionsController.js
+++ controllers/electionsController.js
@@ -51,18 +51,55 @@ var controller = {
     },
 
     getAllElectionsCandidates: (req, res) => {
-        db.query(`SELECT * FROM elections e
+        db.query(`SELECT e.*, c.*, ec.election_id AS candidate_election, p.party_name FROM elections e
             LEFT JOIN election_candidates ec
                 ON e.election_id = ec.election_id
-            LEFT JOIN candidates c
-                on ec.candidate_id = c.candidate_id
-            GROUP BY e.election_id`, (err, results) => {
+            LEFT JOIN candidates c 
+                ON ec.candidate_id = c.candidate_id
+            LEFT JOIN parties p
+                ON e.party_id = p.party_id
+            ORDER BY e.election_id;`, (err, results) => {
                 if (err) {
                     console.log(err);
                     res.send(err);
                 }
                 else {
-                    res.json(results);
+                    var elections = [];
+                    let j = 0;
+                    for (let i = 0; i < results.length; i++) {
+                        if (results[i-1] != null && results[i].election_id == results[i-1].election_id) {
+                            continue;
+                        }
+                        elections[j] = {};
+                        elections[j].electionID = results[i].election_id;
+                        elections[j].date = results[i].date;
+                        elections[j].type = results[i].type;
+                        if (results[i].type == 'primary') {
+                            elections[j].party = results[i].party_name;
+                        }
+                        elections[j].office = results[i].office_id;
+                        if (results[i].district != null) {
+                            elections[j].district = results[i].district;
+                        }
+                        j++;
+                    }
+                    for (let i = 0; i < results.length; i++) {
+                        if (results[i].candidate_id) {
+                            let k = elections.findIndex(x => x.electionID == results[i].candidate_election);
+                            if (!("candidates" in elections[k])) {
+                                elections[k].candidates = [];
+                            }
+                            let newCandidate = {
+                                firstName: results[i].first_name,
+                                lastName: results[i].last_name,
+                                party: results[i].party_name,
+                                website: results[i].website
+                            }
+                            elections[k].candidates.push(newCandidate);
+
+                        }
+                    }
+                    res.send(elections);
                 }
             })
     }
