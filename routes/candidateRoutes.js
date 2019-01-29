74de850b5ee52db988683d6576e7e15b359d2e46
Chris Frashure <csfrashure@gmail.com>
2019-01-28 22:11:17 -0500

    cleaned up routes, added some params

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/candidatesController');
 
-router.get('/', controller.getAllCandidates);
-router.get('/elections', controller.getCandidatesAndTheirElections);
-router.get('/address/:address', controller.getCandidatesByAddress);
-router.get('/:office', controller.getCandidatesByOffice);
-router.get('/:office/:district', controller.getCandidatesByDistrict);
+router.get('/', controller.getAllCandidates); // need to remove * from select, tidy up return data
+router.get('/elections', controller.getCandidatesAndTheirElections); // should be done
+router.get('/address/:address', controller.getCandidatesByAddress); // need to parse district information to use in SQL select
+router.get('/office/:office', controller.getCandidatesByOffice); // 
+router.get('/office/:office/district/:district', controller.getCandidatesByDistrict); // TODO: append elections to candidate object
 
 module.exports = router;
