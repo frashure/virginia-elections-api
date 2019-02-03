bc2765dcb744bc8a080dbadc557a9a498e70d6fd
Chris Frashure <csfrashure@gmail.com>
2019-02-02 19:12:50 -0500

    added additional routes for getting candidates in elections

--- routes/electionRoutes.js
+++ routes/electionRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const controller = require('../controllers/electionsController');
 
 router.get('/', controller.getAllElections);
-router.get('/candidates', controller.getAllElectionsCandidates);
 router.get('/office/:office', controller.getElectionsByOffice);
 router.get('/office/:office/district/:district', controller.getElectionsByDistrict);
 router.get('/year/:year', controller.getElectionsByYear);
+router.get('/candidates', controller.getAllElectionsCandidates);
+router.get('/candidates/office/:office', controller.getCandidatesByOffice);
+router.get('/candidates/office/:office/district/:district', controller.getCandidatesByDistrict);
 
 /*
     TODO: get elections by congressional district,
