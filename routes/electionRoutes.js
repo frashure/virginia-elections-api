dc4a2602aea457df326f0d4b7fbc40836884cf48
Chris Frashure <csfrashure@gmail.com>
2019-02-07 21:38:30 -0500

    added route for getting elections by address

--- routes/electionRoutes.js
+++ routes/electionRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/electionsController');
 
 router.get('/', controller.getAllElections);
+router.get('/address/:address', controller.getElectionsCandidatesByAddress);
 router.get('/office/:office', controller.getElectionsByOffice);
 router.get('/office/:office/district/:district', controller.getElectionsByDistrict);
 router.get('/year/:year', controller.getElectionsByYear);
