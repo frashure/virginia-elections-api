4c548626e3af53976cf01d695c0a378ab3b1a522
Chris Frashure <csfrashure@gmail.com>
2019-02-10 16:41:27 -0500

    moved root route below other routes to fix routing error

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const candidates = require('./candidateRoutes');
 const elections = require('./electionRoutes');
 
-router.use('/', (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
 router.use('/candidates', candidates);
 router.use('/elections', elections);
+router.use('/', (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
 
 module.exports = router;
