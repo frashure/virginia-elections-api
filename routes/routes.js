4a4ae4952ac65c23a24dd1fc8537f025acdb81a9
Chris Frashure <csfrashure@gmail.com>
2019-02-10 18:06:01 -0500

    added districts route

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const candidates = require('./candidateRoutes');
 const elections = require('./electionRoutes');
+const districts = require('./districtRoutes');
 
 router.use('/candidates', candidates);
 router.use('/elections', elections);
+router.use('/districts', districts);
 router.use('/', (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
 
 module.exports = router;
