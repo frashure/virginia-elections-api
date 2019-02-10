fd1b25cb86a1b1ec2e133e93ff6bdf958aa22698
Chris Frashure <csfrashure@gmail.com>
2019-02-09 20:59:13 -0500

    added helmet

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const env = require('dotenv').config();
+const helmet = require('helmet');
 
 let port = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.listen(port, () => {
     console.log('Virginia Elections API server listening on port ' + port);
