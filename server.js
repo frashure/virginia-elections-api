91eddb74de552765d4c7cc024db208aece755d8d
Chris Frashure <csfrashure@gmail.com>
2019-04-10 16:12:57 -0400

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const bodyparser = require('body-parser');
 const env = require('dotenv').config();
 const helmet = require('helmet');
+const cors = require('cors');
 
 let port = process.env.PORT || 3000;
 
 app.use(helmet());
+app.use(cors());
 
 app.listen(port, () => {
     console.log('Virginia Elections API server listening on port ' + port);
