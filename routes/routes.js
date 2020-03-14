b7a33aad43b5f71a9359f3a388a81eec48b57f53
Chris Frashure <csfrashure@gmail.com>
2020-03-14 14:37:22 -0400

    configured CORS for LPLC route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const cors = require('cors');
 
 const candidates = require('./candidateRoutes');
 const elections = require('./electionRoutes');
