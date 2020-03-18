fa775effbb26ac0ca4917dbbfb8bafd3a19b4e9f
Chris Frashure <csfrashure@gmail.com>
2020-03-18 17:08:59 -0400

    removed cors options

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const cors = require('cors');
 
 const candidates = require('./candidateRoutes');
 const elections = require('./electionRoutes');
@@ -10,22 +9,22 @@ const lploudoun = require('../controllers/lpLoudoun');
 
 var test = path.join(__dirname, '../test/test.html');
 
-var whitelist = ['127.0.0.1:8080']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['127.0.0.1:8080']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 router.use('/candidates', candidates);
 router.use('/elections', elections);
 router.use('/districts', districts);
 router.use('/test', express.static(test));
 router.use('/lploudoun/first/:fName/last/:lName/email/:email', lploudoun);
-router.use('/', cors(corsOptions), (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
+router.use('/', (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
 
 module.exports = router;
