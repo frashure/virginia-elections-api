30fff5d158dbba33eb6725544578b8e51e3c2b6a
Chris Frashure <csfrashure@gmail.com>
2020-07-07 19:31:30 -0400

    implemented get on members list, restructured routes

--- controllers/lpLoudoun.js
+++ controllers/lpLoudoun.js
@@ -3,6 +3,15 @@ const request = require('request');
 
 const controller = {
 
+    checkKey: (req, res, next) => {
+        if (req.body.key === process.env.KEY) {
+            return next()
+        }
+        else {
+            res.send(403)
+        }
+    },
+
     signup: (req, res) => {
         db.query(`INSERT INTO lplc (firstName, lastName, email, resident, signupDate) VALUES (?, ?, ?, ?, now())`, [req.body.fName, req.body.lName, req.body.email, req.body.resident], (err, results) => {
             if (err) {
@@ -10,31 +19,31 @@ const controller = {
                 res.send(err);
             }
             else {
-                res.set('Access-Control-Allow-Origin', '*');
-                res.status(200);
-                let resBody = {
-                    "resCode": 2000
-                };
-                res.send(resBody);
+                // res.set('Access-Control-Allow-Origin', '*');
+                // res.status(200);
+                // let resBody = {
+                //     "resCode": 2000
+                // };
+                res.status(200).send();
                 }
         });
     },
 
     list: (req, res) => {
-        db.query('SELECT * FROM lplc', (err, res) => {
+        db.query('SELECT * FROM lplc', (err, results) => {
             var members = [];
             if (err) {
                 console.log(err);
                 res.send(err);
             }
             else {
-                for (let i = 1; i < res.length; i++) {
+                for (let i = 1; i < results.length; i++) {
                     person = {
-                        firstName: res.firstName,
-                        lastName: res.lastName,
-                        email: res.email,
-                        resident: res.resident,
-                        date: res.signupDate
+                        firstName: results[i].firstName,
+                        lastName: results[i].lastName,
+                        email: results[i].email,
+                        resident: results[i].resident,
+                        date: results[i].signupDate
                     }
                     members.push(person);
                 }
--- routes/routes.js
+++ routes/routes.js
@@ -24,18 +24,9 @@ router.use('/candidates', candidates);
 router.use('/elections', elections);
 router.use('/districts', districts);
 router.use('/test', express.static(test));
-router.use('/lploudoun', lploudoun.signup);
-// router.get('/lploudoun', checkKey(), lploudoun.list)
-router.use('/lploudoun/first/:fName/last/:lName/email/:email', lploudoun.signup);
+router.post('/lploudoun', lploudoun.signup);
+router.get('/lploudoun', lploudoun.checkKey, lploudoun.list)
+// router.post('/lploudoun/first/:fName/last/:lName/email/:email', lploudoun.signup);
 router.use('/', (req, res) => {res.redirect('https://github.com/frashure/virginia-elections-api')});
-
-function checkKey(req, res, next) {
-    if (req.body.key === process.env.KEY) {
-        next()
-    }
-    else {
-        res.send(403)
-    }
-}
 
 module.exports = router;
